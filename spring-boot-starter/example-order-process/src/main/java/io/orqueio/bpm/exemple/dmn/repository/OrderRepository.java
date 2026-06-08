5b35e4e477e0c3a309295988a60b771375aca525
Shaymabenahmed-tech <shayma.benahmed@codeonce.fr>
2026-06-08 17:21:15 +0100

    fix(order-example-process): use findFirst to handle duplicate orderId

--- spring-boot-starter/example-order-process/src/main/java/io/orqueio/bpm/exemple/dmn/repository/OrderRepository.java
+++ spring-boot-starter/example-order-process/src/main/java/io/orqueio/bpm/exemple/dmn/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Order findByOrderId(String orderId);
+    Order findFirstByOrderId(String orderId);
 }
